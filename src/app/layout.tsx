0992de8eefc6a3487989890b883e250f08b36e28
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-07-09 21:22:44 +0700

    feature: change name navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../styles/globals.css'
-import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import React, { Suspense } from 'react'
 import { siteConfig } from '@/utils/constants'
 import { CircularProgress } from '@nextui-org/react'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 

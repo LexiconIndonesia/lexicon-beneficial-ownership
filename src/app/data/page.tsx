547b38f697a12bbf34166245ed197a57900a69d7
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-07-09 21:51:47 +0700

    fix(mobile): Mobile optimization for every page

--- src/app/data/page.tsx
+++ src/app/data/page.tsx
@@ -30,22 +30,39 @@ export default function DataPage (): React.ReactElement {
 
   const handleSearchClicked = (): void => {
     const newParams = new URLSearchParams(params)
-    newParams.set('query', query)
-    newParams.set('from', from)
-    newParams.set('to', to)
+
+    newParams.delete('query')
     newParams.delete('subjects')
     newParams.delete('types')
     newParams.delete('nation')
+    newParams.delete('query')
+    newParams.delete('from')
+    newParams.delete('to')
+
+    if (query.length > 0) {
+      newParams.set('query', query)
+    }
+
+    if (from.length > 0) {
+      newParams.set('from', from)
+    }
+
+    if (to.length > 0) {
+      newParams.set('to', to)
+    }
+
     Array.from((subjects ?? []).values()).forEach((subject) => {
       newParams.append('subjects', subject)
     })
+
     Array.from((types ?? []).values()).forEach((subject) => {
       newParams.append('types', subject)
     })
+
     Array.from((nations ?? []).values()).forEach((subject) => {
       newParams.append('nations', subject)
     })
-    if (query.length === 0) newParams.delete('query')
+
     router.replace(`${path}?${newParams.toString()}`, { scroll: false })
   }
 
@@ -53,7 +70,7 @@ export default function DataPage (): React.ReactElement {
     <main className="flex flex-col">
       <div className='py-16'>
         <img src="/images/img_background_small.png" alt="Background Hero" className='absolute top-0 -z-10 mt-2 hidden sm:block' />
-        <div className='w-auto mx-[150px] py-4 px-5 bg-white border border-colorBorder rounded-xl flex flex-row gap-6 z-10 items-center'>
+        <div className='w-auto mx-4 sm:mx-[150px] py-4 px-5 bg-white border border-colorBorder rounded-xl flex flex-row gap-6 z-10 items-center'>
           <div className='w-full flex flex-col gap-3'>
             <input
               className='outline-none w-full font-normal text-sm placeholder-textGray40'
@@ -63,16 +80,8 @@ export default function DataPage (): React.ReactElement {
             />
             <Divider className='border border-colorBorder' />
             <div className='flex flex-col sm:flex-row gap-6'>
-              <FilterSubjectType
-                onSelectedSubjects={(subjects: string[]) => {
-                  setSubjects(subjects)
-                }}
-              />
-              <FilterType
-                onSelectedTypes={(types: string[]) => {
-                  setTypes(types)
-                }}
-              />
+              <FilterSubjectType onSelectedSubjects={(subjects: string[]) => { setSubjects(subjects) }} />
+              <FilterType onSelectedTypes={(types: string[]) => { setTypes(types) }} />
               <FilterNation onSelectedNations={(nations) => { setNations(nations) }} />
               <FilterYear setSelectedYear={(from: string, to: string) => { setFrom(from); setTo(to) }} />
               <Button onClick={handleSearchClicked} className='block sm:hidden bg-colorPrimary text-md font-semibold text-white'>
@@ -85,9 +94,9 @@ export default function DataPage (): React.ReactElement {
           </Button>
         </div>
       </div>
-      <div className='flex flex-col py-12 items-center px-[120px] bg-white gap-8'>
+      <div className='flex flex-col py-12 items-center px-4 sm:px-[120px] bg-white gap-8'>
         <OptionalRendering condition={total > 0}>
-          <h3 className='px-[177px] text-2xl text-center font-semibold'>
+          <h3 className='px-0 sm:px-[177px] text-2xl text-center font-semibold'>
             {total} entities in all subject types, all record types, all nationalities, and every year.
           </h3>
         </OptionalRendering>

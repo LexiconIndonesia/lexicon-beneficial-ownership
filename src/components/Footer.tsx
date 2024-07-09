d2c13673f0c52f9983fb4f9874159e7aaff78b8e
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-07-09 21:28:27 +0700

    fix: resolve conflict

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,48 +2,16 @@ import Image from 'next/image'
 import React from 'react'
 
 export default function Footer (): React.ReactElement {
-  const sponsors = [
-    {
-      name: 'Accountability Lab',
-      imageSrc: '/images/sponsors/AL.png',
-      imageSize: {
-        width: 150,
-        height: 100
-      }
-    },
-    {
-      name: 'CIPE',
-      imageSrc: '/images/sponsors/CIPE.png',
-      imageSize: {
-        width: 120,
-        height: 100
-      }
-    },
-    {
-      name: 'Development Gateway',
-      imageSrc: '/images/sponsors/DG_IREX.png',
-      imageSize: {
-        width: 150,
-        height: 100
-      }
-    }
-  ]
-
   return (
-    <footer className="px-4 sm:px-12 py-8 bg-[#fff4fb]">
-      <h3 className="text-center font-semibold">Supported by</h3>
-      <div className="flex flex-wrap gap-10 justify-center items-center mt-8">
-        {sponsors.map((sponsor, index) => {
-          return (
-            <Image
-              key={index}
-              width={sponsor.imageSize.width}
-              height={sponsor.imageSize.height}
-              src={sponsor.imageSrc}
-              alt={sponsor.name}
-            />
-          )
-        })}
+    <footer className="sm:px-32 sm:py-12 px-6 py-10 bg-lightGray">
+      <div className='w-full sm:w-3/5 flex flex-col'>
+        <Image src={'/images/img_logo.png'} width={155} height={40} alt='Lexicon Logo' />
+        <p className='text-textGray text-xs font-normal mt-5'>
+          LexiconBO is a tool designed to help procurement officials quickly find individuals or companies with a history of fraud and corruption by utilizing advanced keyword and filter systems, making the background check process efficient and reliable.
+        </p>
+        <p className='mt-12 text-textGray40 text-sm'>
+          © {new Date().getFullYear()} LexiconBO. All rights reserved.
+        </p>
       </div>
     </footer>
   )

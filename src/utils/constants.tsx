50cb34cc1bcdf6ccd231f09df5e7418da5521a9e
Naufal Fawwaz <fawaznaufal23@gmail.com>
2024-08-12 21:54:09 +0700

    fix: Naming changes

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,8 +1,8 @@
 export const siteConfig = {
   url: "https://bo.lexicon.id",
-  name: "Lexicon Beneficiary Ownership",
+  name: "Lexicon Beneficial Ownership",
   description:
-    "Beneficiary Ownership is a dedicated company to transparency and integrity in the corporate world. Our mission is to uncover and display the true beneficial ownership of companies, highlighting those involved in corruption. By providing clear, accessible information, we empower citizens, regulators, and investors to make informed decisions. Join us in our quest for accountability and help us shine a light on the hidden faces behind corporate corruption. Discover the truth with Beneficiary Ownership.",
+    "Lexicon Beneficial Ownership is a dedicated company to transparency and integrity in the corporate world. Our mission is to uncover and display the true beneficial ownership of companies, highlighting those involved in corruption. By providing clear, accessible information, we empower citizens, regulators, and investors to make informed decisions. Join us in our quest for accountability and help us shine a light on the hidden faces behind corporate corruption. Discover the truth with Lexicon Beneficial Ownership.",
   owner: "Lexicon",
   ownerUrl: "https://lexicon.id",
 };

b5c8ce28d0e68731693ed4ea17eb49ac1086cac5
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-10-05 15:54:32 +0700

    feature(visualization): Add visualization example

--- src/app/visualization/page.tsx
+++ src/app/visualization/page.tsx
@@ -1,7 +1,50 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
+import dynamic from 'next/dynamic'
+import 'chart.js/auto'
+
+const Bar = dynamic(async () => await import('react-chartjs-2').then((mod) => mod.Bar), {
+  ssr: false
+})
 
 export default function VisualizationPage (): React.ReactElement {
+  const [data] = useState({
+    labels: ['Indonesia', 'Singapore', 'Malaysia', 'Global'],
+    datasets: [
+      {
+        label: 'Cases',
+        data: [9521, 3, 8, 1],
+        backgroundColor: [
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgb(40, 100, 90, 0.2)'
+        ],
+        borderColor: [
+          'rgb(255, 159, 64)',
+          'rgb(255, 99, 132)',
+          'rgb(255, 205, 86)',
+          'rgb(40, 100, 90)'
+        ],
+        borderWidth: 1
+      }
+    ]
+  })
+
   return (
-    <div>VisualizationPage</div>
+    <main className='py-8 flex flex-col justify-center items-center'>
+      <div className='py-16'>
+        <img src="/images/img_background_small.png" alt="Background Hero" className='absolute top-0 left-0 right-0 -z-10 mt-2 hidden sm:block' />
+        <div className='flex flex-col items-center bg-white z-10'>
+          <h1 className='text-[40px] font-semibold'>Cases Insights</h1>
+          <h5 className='text-[16px] font-normal text-gray-500'>Interactive Data Visualizations of Case Trends and Insights</h5>
+        </div>
+      </div>
+      <div className='flex flex-col w-full px-32 py-16 gap-4'>
+        <h3 className='text-2xl font-semibold'>Cases by Nations</h3>
+        <Bar className='mt-10' data={data} options={{ plugins: { legend: { display: false } } }} />
+      </div>
+    </main>
   )
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,42 @@
 import Image from 'next/image'
 import React from 'react'
+import InstagramIcon from './icons/InstagramIcon'
+import LinkedInIcon from './icons/LinkedInIcon'
+import GithubIcon from './icons/GithubIcon'
 
 export default function Footer (): React.ReactElement {
   return (
-    <footer className="sm:px-32 sm:py-12 px-6 py-10 bg-lightGray">
+    <footer className="sm:px-32 sm:py-12 px-6 py-10 bg-lightGray flex sm:flex-row flex-col gap-4">
       <div className='w-full sm:w-3/5 flex flex-col'>
         <Image src={'/images/img_logo.png'} width={155} height={40} alt='Lexicon Logo' />
         <p className='text-textGray text-xs font-normal mt-5'>
           LexiconBO is a tool designed to help procurement officials quickly find individuals or companies with a history of fraud and corruption by utilizing advanced keyword and filter systems, making the background check process efficient and reliable.
         </p>
+        <ul className='flex flex-row gap-2 mt-4'>
+          <li>
+            <a href="https://www.instagram.com/lexiconsearch/" target='_blank' rel="noreferrer" className='cursor-pointer hover:opacity-80 transition-all duration-200'>
+              <InstagramIcon />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/company/lexicon-indo" target='_blank' rel="noreferrer" className='cursor-pointer hover:opacity-80 transition-all duration-200'>
+              <LinkedInIcon />
+            </a>
+          </li>
+        </ul>
         <p className='mt-12 text-textGray40 text-sm'>
           © {new Date().getFullYear()} LexiconBO. All rights reserved.
         </p>
+      </div>
+      <div className='flex flex-col items-end flex-1'>
+        <a
+          href="https://github.com/LexiconIndonesia"
+          target='_blank'
+          className='flex flex-row justify-center items-center gap-2 bg-gray-200 hover:opacity-80 transition-all duration-200 px-4 py-2 rounded-lg'
+          rel="noreferrer">
+          <GithubIcon />
+          <span className='text-sm font-semibold'>Let&apos;s Contribute!</span>
+        </a>
       </div>
     </footer>
   )
